7e1e3acb8dc442ba6854bc5d2387c3e0a75082e2
jing277272 <jing277272@163.com>
2021-02-23 17:25:19 +0800

    master

--- manuf/urls.py
+++ manuf/urls.py
@@ -17,11 +17,6 @@ from django.contrib import admin
 from django.urls import path
 from django.conf.urls import url, include
 
-
-
-
-
-
 urlpatterns = [
     
     #url(r'app01/', include('app01.urls')),
