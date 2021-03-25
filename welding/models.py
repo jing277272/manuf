9751df1a8b033791f4de3148812f5b685b600f9d
jing277272 <jing277272@163.com>
2021-03-25 09:52:08 +0800

    master

--- welding/models.py
+++ welding/models.py
@@ -4,25 +4,39 @@ from datetime import datetime
 
 
 class Parameters(models.Model):
-    equipment = models.CharField(max_length=20, blank=True, null=True, verbose_name="设备")
-    serial= models.CharField(max_length=3, blank=True, null=True, verbose_name="系列")
-    squeeze= models.CharField(max_length=3, blank=True, null=True, verbose_name="预压")
-    slope= models.CharField(max_length=3, blank=True, null=True, verbose_name="缓升")
-    weld1= models.CharField(max_length=3 ,blank=True, null=True, verbose_name="通电1")
-    cool1= models.CharField(max_length=3, blank=True, null=True, verbose_name="冷却1")
-    weld2= models.CharField(max_length=3, blank=True, null=True, verbose_name="通电2")
-    cool2= models.CharField(max_length=3, blank=True, null=True, verbose_name="冷却2")
-    weld3= models.CharField(max_length=3, blank=True, null=True, verbose_name="通电3")
-    dslope= models.CharField(max_length=3, blank=True, null=True, verbose_name="缓降")
-    hold= models.CharField(max_length=3, blank=True, null=True, verbose_name="保持")
-    heat1= models.CharField(max_length=3, blank=True, null=True, verbose_name="焊接1")
-    heat2= models.CharField(max_length=3, blank=True, null=True, verbose_name="焊接2")
-    heat3= models.CharField(max_length=3, blank=True, null=True, verbose_name="焊接3")
-    gauge= models.CharField(max_length=3, blank=True, null=True, verbose_name="表压力")
-    pressure= models.CharField(max_length=3, blank=True, null=True, verbose_name="加压力")
-    part=models.ManyToManyField(to="Recipe")
-											
-    
+    equipment = models.CharField(
+        max_length=20, blank=True, null=True, verbose_name="设备")
+    serial = models.CharField(max_length=3, blank=True,
+                              null=True, verbose_name="系列")
+    squeeze = models.CharField(
+        max_length=3, blank=True, null=True, verbose_name="预压")
+    slope = models.CharField(max_length=3, blank=True,
+                             null=True, verbose_name="缓升")
+    weld1 = models.CharField(max_length=3, blank=True,
+                             null=True, verbose_name="通电1")
+    cool1 = models.CharField(max_length=3, blank=True,
+                             null=True, verbose_name="冷却1")
+    weld2 = models.CharField(max_length=3, blank=True,
+                             null=True, verbose_name="通电2")
+    cool2 = models.CharField(max_length=3, blank=True,
+                             null=True, verbose_name="冷却2")
+    weld3 = models.CharField(max_length=3, blank=True,
+                             null=True, verbose_name="通电3")
+    dslope = models.CharField(max_length=3, blank=True,
+                              null=True, verbose_name="缓降")
+    hold = models.CharField(max_length=3, blank=True,
+                            null=True, verbose_name="保持")
+    heat1 = models.CharField(max_length=3, blank=True,
+                             null=True, verbose_name="焊接1")
+    heat2 = models.CharField(max_length=3, blank=True,
+                             null=True, verbose_name="焊接2")
+    heat3 = models.CharField(max_length=3, blank=True,
+                             null=True, verbose_name="焊接3")
+    gauge = models.CharField(max_length=3, blank=True,
+                             null=True, verbose_name="表压力")
+    pressure = models.CharField(
+        max_length=3, blank=True, null=True, verbose_name="加压力")
+    part = models.ManyToManyField(to="Recipe")
 
     class Meta:
         db_table = "w_parameters"
@@ -30,22 +44,35 @@ class Parameters(models.Model):
 
 class Recipe(models.Model):
 
-    item = models.CharField(max_length=20, blank=True, null=True, verbose_name="项目")
-    part_no = models.CharField(max_length=20, blank=True, null=True, verbose_name="零件号")
-    op = models.CharField(max_length=20, blank=True,null=True, verbose_name="工序")
-    name = models.CharField(max_length=20, blank=True,null=True, verbose_name="名称")
-    sc = models.ManyToManyField(to='Parameters',verbose_name="参数")
-    m1 = models.CharField(max_length=20, blank=True, null=True, verbose_name="材料1")
-    t1= models.CharField(max_length=20, blank=True, null=True, verbose_name="厚度1")
-    m2= models.CharField(max_length=20, blank=True, null=True, verbose_name="材料2")
-    t2= models.CharField(max_length=20, blank=True, null=True, verbose_name="厚度2")
-    m3= models.CharField(max_length=20, blank=True, null=True, verbose_name="材料3")
-    t3= models.CharField(max_length=20, blank=True, null=True, verbose_name="厚度3")
+    item = models.CharField(max_length=20, blank=True,
+                            null=True, verbose_name="项目")
+    part_no = models.CharField(
+        max_length=20, blank=True, null=True, verbose_name="零件号")
+    op = models.CharField(max_length=20, blank=True,
+                          null=True, verbose_name="工序")
+    name = models.CharField(max_length=20, blank=True,
+                            null=True, verbose_name="名称")
 
-    diameter = models.CharField(max_length=20, blank=True, null=True, verbose_name="修磨直径（mm)")
-    frequency = models.CharField(max_length=20, blank=True, null=True, verbose_name="修磨频次")
-    modify = models.ForeignKey(blank=False, null=True, default='1', to='web.UserInfo', on_delete=models.CASCADE,verbose_name="修改者")
+    m1 = models.CharField(max_length=20, blank=True,
+                          null=True, verbose_name="材料1")
+    t1 = models.CharField(max_length=20, blank=True,
+                          null=True, verbose_name="厚度1")
+    m2 = models.CharField(max_length=20, blank=True,
+                          null=True, verbose_name="材料2")
+    t2 = models.CharField(max_length=20, blank=True,
+                          null=True, verbose_name="厚度2")
+    m3 = models.CharField(max_length=20, blank=True,
+                          null=True, verbose_name="材料3")
+    t3 = models.CharField(max_length=20, blank=True,
+                          null=True, verbose_name="厚度3")
+    diameter = models.CharField(
+        max_length=20, blank=True, null=True, verbose_name="修磨直径（mm)")
+    frequency = models.CharField(
+        max_length=20, blank=True, null=True, verbose_name="修磨频次")
+    modify = models.ForeignKey(blank=False, null=True, default='1',
+                               to='web.UserInfo', on_delete=models.CASCADE, verbose_name="修改者")
     modify_time = models.DateTimeField(auto_now_add=True, verbose_name="修改时间")
+    sc = models.ManyToManyField(to='Parameters', verbose_name="参数")
 
     def __str__(self):
         return "%s-%s" % (self.part_no, self.op)
