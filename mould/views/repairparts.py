5ab15173864c401583295fb418b9bf7d979d7fbd
jing277272 <jing277272@163.com>
2021-03-27 17:46:47 +0800

    master

--- mould/views/repairparts.py
+++ mould/views/repairparts.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render
-
+from mould.forms.repairparts import RepairpartsModelForm
+from utils.pagination import Pagination
+from mould.models import Mould,RepairParts
 
 def index(request):
     return render(request, 'web/index.html')
@@ -7,28 +9,35 @@ def index(request):
 
 # 在库备件查询
 def r_parts(request, tool_id):
+    if request.method == 'GET':
     # 分页获取数据
-    queryset = RepairParts.objects.filter(tool_id=tool_id)
-    mould = Mould.objects.filter(id=tool_id).values('name')
-    print(mould)
-    if queryset:
-        page_object = Pagination(
-            current_page=request.GET.get('page'),
-            all_count=queryset.count(),
-            base_url=request.path_info,
-            query_params=request.GET,
-        )
+        queryset = RepairParts.objects.filter(tool_id=tool_id,quantity__gt=0)
+        moulds = Mould.objects.filter(id=tool_id).values('name')
+        print(moulds)
+        if queryset:
+            page_object = Pagination(
+                current_page=request.GET.get('page'),
+                all_count=queryset.count(),
+                base_url=request.path_info,
+                query_params=request.GET,
+                )
 
-        mould_object_list = queryset[page_object.start:page_object.end]
+            mould_object_list = queryset[page_object.start:page_object.end]
 
-        context = {
-            'mould': mould,
+            context = {
+                'mould': moulds,
+                'search_result': mould_object_list,
+                'page_html': page_object.page_html(),
+                }
+            return render(request, 'mould/repair.html', context)
 
-            'search_result': mould_object_list,
-            'page_html': page_object.page_html(),
-
-        }
-        return render(request, 'mould/repair.html', context)
-
-    error_msg = "抱歉！没有备件在库信息！"
-    return render(request, 'mould/repair.html', error_msg)
+        error_msg = "出错了！没有备件在库信息！"
+        return render(request, 'mould/repair.html', locals())
+    form = RepairpartsModelForm(request, data=request.POST)
+    if form.is_valid():
+        
+        
+        # 保存
+        form.save()
+        return JsonResponse({'status': True, })
+    return JsonResponse({'status': False, 'error': form.errors})
