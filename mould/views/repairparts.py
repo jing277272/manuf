d3c72aa2e06f3184c7bcc3a6282e03b50c2c7bd5
jing277272 <jing277272@163.com>
2021-04-19 11:38:25 +0800

    master

--- mould/views/repairparts.py
+++ mould/views/repairparts.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render , redirect
 from mould.forms.repairparts import RepairpartsModelForm
 from utils.pagination import Pagination
 from django.http import HttpResponse, JsonResponse, request
@@ -16,6 +16,7 @@ def r_parts(request, tool_id):
         queryset = RepairParts.objects.filter(tool_id=tool_id, quantity__gt=0)
         mould_object = Mould.objects.filter(id=tool_id).first()  # .values('name')
         '''print(tool_id)'''
+        form = RepairpartsModelForm(request)
 
         if queryset:
             page_object = Pagination(
@@ -29,7 +30,7 @@ def r_parts(request, tool_id):
             rparts_object_list = queryset[page_object.start:page_object.end]
             print(mould_object)
 
-            form = RepairpartsModelForm(request)
+            
 
             context = {
                 'tool_id': tool_id,
@@ -42,16 +43,25 @@ def r_parts(request, tool_id):
 
         error_msg = "出错了！没有备件在库信息！"
 
+
         return render(request, 'mould/repair.html', locals())
      # 备件出库
     print('Post')
-    tool_id= request.POST.get('tool_id')
-    quantity=  request.POST.get('quantity')
-    print(tool_id)
-    if quantity :
-        RepairParts.objects.filter(id=tool_id).update(quantity=quantity)
-        return JsonResponse({'status': True, })
-    return JsonResponse({'status': False, 'error': form.errors})
+    tool_id = request.POST.get('tool_id')
+    rpartid= request.POST.get('rpartid')
+    used=  request.POST.get('use')
+    print(used)
+    oldquantity = RepairParts.objects.get(id=rpartid).quantity
+    
+    newquantity= int(oldquantity) - int(used)
+    print(newquantity)
+    RepairParts.objects.filter(id=rpartid).update(quantity=newquantity)
+    context = {
+                'tool_id': tool_id,
+            }
+
+    return redirect('r_parts',tool_id=tool_id,)
+    
 
 
 def add_rpart(request, tool_id):
